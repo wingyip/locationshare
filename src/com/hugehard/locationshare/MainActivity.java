90055f2b0f1a0089c974ef02736ed2789a6d9dd1
wingyip <xo9287@sina.com>
2013-12-14 18:34:32 +0800

    add activity

---
+++ src/com/hugehard/locationshare/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hugehard.locationshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hugehard.find.R;
+
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+}
